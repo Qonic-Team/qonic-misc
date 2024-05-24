1d4704730dc85a71492360c89a36b07b065c0a6b
cogrpar <owen.r.welsh@gmail.com>
2024-05-24 14:04:18 -0600

    updated requirements for security

--- source_dir/setup.py
+++ source_dir/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name = 'qonic_misc',
     packages = ['qonic_misc'],
-    version = '0.1.5',
+    version = '0.1.6',
     description = 'Python library with miscellaneous tools to be used in conjunction with the qonic framework',
     author = 'cogrpar',
     author_email = 'owen.r.welsh@hotmail.com',
@@ -12,5 +12,5 @@ setup(
     license='Apache License 2.0',
     keywords = ['qonic', 'qonic_misc'],
     setup_requires=['wheel'],
-    install_requires=['numpy>=1.19.2', 'tensorflow>=2.4.0']
+    install_requires=['numpy>=1.22.2 ', 'tensorflow>=2.12.0rc1', 'keras>=2.13.1rc0', 'setuptools>=65.5.1', 'werkzeug>=3.0.3', 'wheel>=0.38.0']
 )
